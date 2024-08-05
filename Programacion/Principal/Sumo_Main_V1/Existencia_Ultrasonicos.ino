673472f331713f0c40d7c9064568bd44d7b5e4b8
Aiden Maid Perez <aiden.maidp@gmail.com>
2024-08-05 17:47:46 -0300

    Codigo nuevo sin terminar

---
+++ Programacion/Principal/Sumo_Main_V1/Existencia_Ultrasonicos.ino
@@ -0,0 +1,33 @@
+// Existencia Ultrasonicos
+
+void ExistenciaUlt()
+{
+  if(distancia_ult_med < 10)
+  {
+    //Serial.println("Hay algo");
+    flag_ult_med = true;
+  }
+  else if(distancia_ult_med > 10)
+  {
+    //Serial.println("No hay moros en la costa");
+    flag_ult_med = false;
+  }
+
+  if(distancia_ult_der < 15)
+  {
+    flag_ult_der = true;
+  }
+  else if(distancia_ult_der > 15)
+  {
+    flag_ult_der = false;
+  }
+
+  if(distancia_ult_izq < 15)
+  {
+    flag_ult_izq = true;
+  }
+  else if(distancia_ult_izq > 15)
+  {
+    flag_ult_izq = false;
+  }
+}
--- Programacion/Principal/Sumo_Main_V1/Lectura_Piso.ino
+++ Programacion/Principal/Sumo_Main_V1/Lectura_Piso.ino
@@ -1,23 +1,49 @@
-// Código Lectura
+// Lectura Piso
 
-uint8_t lectura_cny_der = 0;
-uint8_t lectura_cny_med = 0;
-uint8_t lectura_cny_izq = 0;
+// CNY Izquierdo
+uint32_t lectura_cny_izq = 0;
+uint32_t cny_izquierdo = 0;
+uint32_t suma_cny_izq = 0;
 
-uint8_t medio;
-uint8_t medBlanco = 0;
-uint8_t medNegro = 0;
-uint8_t medPromedio = (medBlanco + medNegro) /2;
+uint32_t izq_blanco = 28;
+uint32_t izq_negro = 735;
 
-uint8_t izquierdo;
-uint8_t izqBlanco = 30;
-uint8_t izqNegro = 1005;
-uint8_t izqPromedio = (izqBlanco + izqNegro) /2;
+uint32_t izq_promedio = (izq_blanco + izq_negro) /2;
 
-uint8_t derecho;
-uint8_t derBlanco = 24;
-uint8_t derNegro = 990;
-uint8_t derPromedio = (derBlanco + derNegro) /2;
+//CNY Derecho
+uint32_t lectura_cny_der = 0;
+uint32_t cny_derecho = 0;
+uint32_t suma_cny_der = 0;
+
+uint32_t der_blanco = 285;
+uint32_t der_negro = 805;
+
+uint32_t der_promedio = (der_blanco + der_negro) /2;
+
+bool flag_cny_der = false;
+bool flag_cny_izq = false;
+bool flag_cny_both = false;
+
+void LecturaCNY()
+{
+  for (int i = 0; i <10; i++)
+  {
+    lectura_cny_izq = analogRead(CNY_IZQ);
+    suma_cny_izq = suma_cny_izq + lectura_cny_izq;
+  }
+
+  for (int i = 0; i <10; i++)
+  {
+    lectura_cny_der = analogRead(CNY_DER);
+    suma_cny_der = suma_cny_der + lectura_cny_der;
+  }
+
+  cny_izquierdo = suma_cny_izq /10;
+  cny_derecho = suma_cny_der /10;
+
+  suma_cny_izq = 0;
+  suma_cny_der = 0;
+}
 
 
 
