e2efa34dc0eda95f35718f003c3f685e116b45a5
Uridyner <tortillas1152@gmail.com>
2024-08-30 20:52:32 -0300

    nose como se hace el sketch main

--- Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
+++ Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
@@ -4,18 +4,10 @@
 #include "botones.h"
 
 
-//BOTONES Y LEDS
-int estado = 0;  // 0-15 (4 bits para los 4 LEDs)
-bool sistemaEncendido = false;
-unsigned long tiempoInicio = 0;
-bool contadorEnMarcha = false;
-
-
 void setup() {
-  configurarBotonesYLeds(A4, A5, A3, 5, 6, 7);  //cambair los pines 5, 6 y 7 por los leds:   A4=Boton 1 / A5=Boton 2 / A3=Led1 / Los pines de los less 2,3,4 estan en el shift register y no los se anotar
+ // configurarBotonesYLeds(A4, A5, A3, 5, 6, 7);  //cambair los pines 5, 6 y 7 por los leds:   A4=Boton 1 / A5=Boton 2 / A3=Led1 / Los pines de los less 2,3,4 estan en el shift register y no los se anotar
 }
 
 void loop() {
-  actualizarEstado(estado, sistemaEncendido, tiempoInicio, contadorEnMarcha);
-  verificarContador(tiempoInicio, contadorEnMarcha, sistemaEncendido);
+
 }
