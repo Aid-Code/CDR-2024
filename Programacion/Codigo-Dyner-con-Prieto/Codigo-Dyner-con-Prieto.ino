1c78cfea6a7226d36a3eadd829f07959cbaa4d8a
Uridyner <152835266+Uridyner@users.noreply.github.com>
2024-08-30 15:06:40 -0300

    coadaigo con agus

--- Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
+++ Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
@@ -1,24 +1,10 @@
 #include "motors.h"
 #include "ultrasonicos.h"
 #include "cny.h"
-
-// Sensores
-//CNY70
-
-//Ultrasonicos
-
-
-// Placa de Botones
-#define BTN A4
-/*#define BTN_1 A6
-#define BTN_2 A7
-#define LED_1 A4
-#define LED_2 A3
-#define LED_3 13
-#define LED_4 A0*/
+#include "botones.h"
 
 void setup() {
-  setDir(ADELANTE, pines_motorDer);
+  
 }
 
 void loop() {
