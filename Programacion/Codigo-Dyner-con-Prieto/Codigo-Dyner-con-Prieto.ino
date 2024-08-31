4573c3da301535cbe0fd504f825c7cdf31d595f4
Uridyner <tortillas1152@gmail.com>
2024-08-31 00:26:39 -0300

    botones

--- Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
+++ Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
@@ -5,29 +5,9 @@
 
 
 void setup() {
-  // ULTRASONICOS
-  pinMode(ECHO_DER, INPUT);
-  pinMode(TRIG_DER, OUTPUT);
-  pinMode(ECHO_IZQ, INPUT);
-  pinMode(TRIG_IZQ, OUTPUT);
-  pinMode(ECHO_MED, INPUT);
-  pinMode(TRIG_MED, OUTPUT);
-
-  //MOTORES
-  pinMode(MotorIzq_A, OUTPUT);
-  pinMode(MotorIzq_B, OUTPUT);
-  pinMode(PWM_Izq, OUTPUT);
-  pinMode(MotorDer_A, OUTPUT);
-  pinMode(MotorDer_B, OUTPUT);
-  pinMode(PWM_Der, OUTPUT);
-
-  //CNY
-  pinMode(CNY_IZQ, INPUT);
-  pinMode(CNY_DER, INPUT);
-
-  Serial.begin(9600);
+ 
 }
 
 void loop() {
-  presencia();
+ 
 }
--- Programacion/Codigo-Dyner-con-Prieto/botones.h
+++ Programacion/Codigo-Dyner-con-Prieto/botones.h
@@ -3,6 +3,23 @@
 
 #include <Arduino.h>
 
+// Pines de los botones y LEDs
+extern int BTN_1;
+extern int BTN_2;
+extern int pinLeds[4];
+
+// Variables globales
+extern int estado;
+extern bool sistemaEncendido;
+
+// Definición del arreglo de estrategias
+extern int estrategias[16];
+
+// Funciones de la librería
+void configurarBotonesYLeds(int boton1, int boton2, int led1, int led2, int led3, int led4);
+void actualizarEstado();
+void iniciarContador();
+void verificarContador();
+void aplicarEstrategia(int estrategia);
 
 #endif  
-
