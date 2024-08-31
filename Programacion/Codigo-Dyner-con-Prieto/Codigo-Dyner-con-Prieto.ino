8f55806cdb166c3144ea18317da18ffcff6bd4d2
Uridyner <tortillas1152@gmail.com>
2024-08-30 21:02:02 -0300

    .

--- Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
+++ Programacion/Codigo-Dyner-con-Prieto/Codigo-Dyner-con-Prieto.ino
@@ -5,7 +5,7 @@
 
 
 void setup() {
- // configurarBotonesYLeds(A4, A5, A3, 5, 6, 7);  //cambair los pines 5, 6 y 7 por los leds:   A4=Boton 1 / A5=Boton 2 / A3=Led1 / Los pines de los less 2,3,4 estan en el shift register y no los se anotar
+ //configurarBotonesYLeds(A4, A5, A3, 5, 6, 7);  //cambair los pines 5, 6 y 7 por los leds:   A4=Boton 1 / A5=Boton 2 / A3=Led1 / Los pines de los less 2,3,4 estan en el shift register y no los se anotar
 }
 
 void loop() {
